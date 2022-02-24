b44af848653f859dc07ecf2c4dd10f4a6ae42c84
Manok727 <96411985+Manok727@users.noreply.github.com>
2022-02-24 08:34:52 +0100

    changes

--- waifu_sim.py
+++ waifu_sim.py
@@ -1,1 +1,114 @@
 import pygame as pg
+from waifu_sprites import *
+ 
+WIDTH = 1920
+HEIGHT = 1080 
+FPS = 60
+ 
+
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+BLUE = (0,0,255)
+RED = (255,0,0)
+ 
+class Game(): 
+    def __init__(self):
+        pg.init()
+ 
+        self.comic_sans30 = pg.font.SysFont("Comic Sans MS", 30)
+ 
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        self.clock = pg.time.Clock()
+ 
+        self.new()
+    
+    def new(self):
+        self.all_sprites = pg.sprite.Group()
+        self.enemies = pg.sprite.Group()
+        self.projectiles = pg.sprite.Group()
+        self.buffs = pg.sprite.Group()
+ 
+        self.my_player = Player(self)
+        self.all_sprites.add(self.my_player)
+ 
+        self.difficulty = -3
+        self.difficulty_amount = 10
+        self.increase_difficulty = False
+ 
+        self.score = 0
+ 
+        self.run()
+ 
+    def run(self):
+        self.playing = True
+        while self.playing:
+            self.clock.tick(FPS)
+            self.events()
+            self.update()
+            self.draw()
+ 
+        self.new()
+ 
+    def events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                self.playing = False
+                pg.quit()
+ 
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_1:
+                    self.playing = False
+                    
+                            
+    def update(self):
+        self.all_sprites.update()
+        self.hits_enemy = pg.sprite.spritecollide(self.my_player, self.enemies, True)
+        if self.hits_enemy:
+            self.my_player.hp -= 1
+ 
+            if self.my_player.hp <= 0:
+                self.my_player.kill()
+                self.playing = False
+ 
+        self.hits_projectile = pg.sprite.groupcollide(self.projectiles, self.enemies, False, False)
+        if self.hits_projectile:
+            self.score += 1
+            
+        self.hits_projectile = pg.sprite.spritecollide(self.my_player, self.buffs, False, False)
+        if self.hits_projectile:
+            self.score += 1
+            
+ 
+        while len(self.enemies) < 10:
+            self.freak = Enemy(self)
+            self.all_sprites.add(self.freak)
+            self.enemies.add(self.freak)
+ 
+        if self.score > self.difficulty_amount:
+            self.increase_difficulty = True
+ 
+        if self.increase_difficulty:
+            self.difficulty_amount += 250
+            self.increase_difficulty = False
+            self.difficulty -= 1
+            
+        if len(self.buffs) < 1:
+            self.buff = Buff(self.my_player, self)
+
+
+
+ 
+    def draw(self):
+        self.screen.fill(WHITE)
+ 
+        self.all_sprites.draw(self.screen)
+ 
+        self.text_player_hp = self.comic_sans30.render(str(self.my_player.hp), False, (RED))
+        self.text_score = self.comic_sans30.render(str(self.score), False, (RED))
+        
+        self.screen.blit(self.text_player_hp, (10, 10))
+        self.screen.blit(self.text_score, (70, 10))
+ 
+        pg.display.update()
+ 
+g = Game()
--- waifu_sprites.py
+++ waifu_sprites.py
@@ -1,1 +1,132 @@
 import pygame as pg
+vec = pg.math.Vector2
+from random import randint
+ 
+enemy_image = pg.image.load("Character_Paimon_Portrait.png")
+player_image = pg.image.load("Traveler_Male_Card.jpg")
+arrow_image = pg.image.load("sticker_5.png")
+atk_buff = pg.image.load("Enemy_Large_Pyro_Slime.png")
+ 
+class Player(pg.sprite.Sprite):
+    def __init__(self, game):
+        pg.sprite.Sprite.__init__(self)
+        self.game = game
+        self.image = player_image
+        self.image = pg.transform.scale(self.image, (75,150))
+        self.rect = self.image.get_rect()
+        self.pos = vec(100, 100)
+        self.rect.center = self.pos
+        self.speed = 10
+        self.hp = 10
+        self.ammo = 30
+    
+    def update(self):
+        keys = pg.key.get_pressed()
+ 
+        if keys[pg.K_w]:
+            self.pos.y -= self.speed
+        if keys[pg.K_s]:
+            self.pos.y += self.speed
+        if keys[pg.K_a]:
+            self.pos.x -= self.speed
+        if keys[pg.K_d]:
+            self.pos.x += self.speed
+        
+        if keys[pg.K_SPACE] and self.ammo >= 1:
+            self.ammo -= 1
+            print("shooting")
+            self.projectile = Projectile(self, self.game)
+        
+        if self.ammo <= 1:
+            self.ammo += 1
+ 
+        print(self.ammo)
+ 
+        self.rect.center = self.pos
+        if self.rect.left < 0:
+            self.rect.left = 0
+ 
+        self.rect.center = self.pos
+ 
+
+
+ 
+class Enemy(pg.sprite.Sprite):
+    def __init__(self, game):
+        pg.sprite.Sprite.__init__(self)
+        self.game = game
+        self.image = enemy_image
+        self.image = pg.transform.scale(self.image, (175,150))
+        self.rect = self.image.get_rect()
+        self.pos = vec(randint(2500, 2600), randint(0, 1200)) 
+        self.rect.center = self.pos
+        self.speed_x = self.game.difficulty
+        self.life = 50
+ 
+        self.increase_difficulty = False
+        self.difficulty_amount = 10
+    
+    def update(self):
+        self.pos.x += self.speed_x
+ 
+        if self.pos.x < -50:
+            self.game.score -= 10
+            self.kill()
+ 
+        self.hits_o = pg.sprite.spritecollide(self, self.game.projectiles, True)
+
+        if self.hits_o:
+            self.life -= 1
+        
+        if self.life < 0:
+            self.kill()
+ 
+        self.rect.center = self.pos
+
+
+
+ 
+class Projectile(pg.sprite.Sprite):
+    def __init__(self, player, game):
+        pg.sprite.Sprite.__init__(self)
+        self.player = player
+        self.game = game
+        self.game.all_sprites.add(self)
+        self.game.projectiles.add(self)
+ 
+        self.image = arrow_image
+        self.image = pg.transform.scale(self.image, (25, 25))
+        self.image.set_colorkey((255,255,255))
+ 
+        self.rect = self.image.get_rect()
+        self.pos = vec(self.player.pos.x, self.player.pos.y) 
+        self.rect.center = self.pos
+        self.speed_x = 10
+        self.damage = 1
+    
+    def update(self):
+        self.pos.x += self.speed_x
+ 
+        self.rect.center = self.pos
+        
+class Buff(pg.sprite.Sprite):
+    def __init__(self,player,game):
+        pg.sprite.Sprite.__init__(self)
+        self.player = player
+        self.game = game
+        self.game.all_sprites.add(self)
+        self.game.buffs.add(self)
+ 
+        self.image = atk_buff
+        self.image = pg.transform.scale(self.image, (25, 25))
+        self.image.set_colorkey((255,255,255))
+ 
+        self.rect = self.image.get_rect()
+        self.pos = vec(500,500) 
+        self.rect.center = self.pos
+        self.speed_x = 10
+        self.damage = 1
+    
+    def update(self):
+ 
+        self.rect.center = self.pos
